13ce02a77c422f51ea406d3cff568a48c2201e4f
May <71680435+maysnowpak@users.noreply.github.com>
2020-10-17 10:34:51 +1100

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,43 +1,48 @@
 class Solution
 
+#test---------------------------
+  def gen(n)
+  n.times.map { rand(8000) + 1 }
+  end
+
+  test = gen(5)
 #1------------------------------
-def caesar_cipher(string, shift)
-  lower = ("a".."z").to_a
-  upper = ("A".."Z").to_a
+  def caesar_cipher(string, shift)
+    lower = ("a".."z").to_a
+    upper = ("A".."Z").to_a
 
-  encrypter = Hash[
+    encrypter = Hash[
     lower.zip(lower.rotate(shift)) + upper.zip(upper.rotate(shift))
-  ]
-
-  string.chars.map { |c| encrypter.fetch(c, c) }.join
-end
+    ]
+    string.chars.map { |c| encrypter.fetch(c, c) }.join 
+  end 
 
 #2------------------------------
-def substrings(text,dictionary) 
-
-words = text.downcase.split(" ") 
-frequencies = Hash.new(0) 
-words.each { |word|
-dictionary.each { |dict_word| 
-frequencies[dict_word] += 1 if word.include?(dict_word)
-}
-} 
- frequencies
- end
+  def substrings(text,dictionary) 
+    words = text.downcase.split(" ") 
+    
+    frequencies = Hash.new(0) 
+    words.each { |word|
+    dictionary.each { |dict_word| 
+    frequencies[dict_word] += 1 if word.include?(dict_word)
+    }
+    } 
+    frequencies
+  end
 
 #3------------------------------
-def stock_picker (array)
-  max_profit = 0
-  best_buy_day = 0
-  best_sell_day = 0
+  def stock_picker (array)
+    max_profit = 0
+    best_buy_day = 0
+    best_sell_day = 0
 
-  for buy_day in 0..(array.length - 2) #indexes outer layer
-    for sell_day in (buy_day + 1)..(array.length - 1)
+    for buy_day in 0..(array.length - 2) 
+      for sell_day in (buy_day + 1)..(array.length - 1)
 
       profit = array[sell_day] - array[buy_day]
 
       if (profit > max_profit)
-        max_profit = profit  # keep updating until the max 
+        max_profit = profit   
         best_buy_day = buy_day
         best_sell_day = sell_day
       end
@@ -45,102 +50,119 @@ def stock_picker (array)
   end
 
   [best_buy_day, best_sell_day]
-
-end
-
+  end
 
 #4------------------------------
-
-def bubble_sort (array) 
-  until array == array.sort
-    for j in 0..(array.length - 2)
-      array[j], array[j+1] = array[j+1], array[j] if array[j] > array[j+1]
+  def bubble_sort(array)
+    until array == array.sort
+      for i in 0..(array.length - 2)
+        if array[i] > array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i]
+        end
+      end
     end
+    array
   end
-    
-  array
-end
-
-
 #5------------------------------
-
-def selection_sort(array)
- for i in 0..(array.length-1)
- min_index = i
-    for j in (i + 1)..(array.length-1)
-      min_index = j if array[j] < array[min_index]
+  def selection_sort(array)
+    last_element = array.length-1
+    for i in 0..last_element
+      min_index = i
+        for j in (i + 1)..last_element
+          min_index = j if array[j] < array[min_index]
+        end
+      array[i], array[min_index] = array[min_index], array[i] if min_index != i #swap when min_index != to i (everytime it changes)
     end
-    array[i], array[min_index] = array[min_index], array[i] if min_index != i #swap 
+    array
   end
-  array
-end
- 
  
 #6------------------------------
-def self,fib_recursive(n)
-  if num < 0 then
-      raise ArgumentError, "The number must be a positive integer"
+  def self.fib_recursive(n)
+    raise ArgumentError, "The number must be a positive integer" if n < 0
+    return [] if n == 0
+    return [0] if n == 1
+    return [0,1] if n == 2 
+  
+    seq = fib_recursive(n - 1)
+    seq << seq[-2] + seq[-1]
+    seq
   end
 
-  return [] if num == 0
-  return [0] if num == 1
-  return [0,1] if num == 2 
-  
-  seq = fib_recursive(num - 1) 
-  seq << seq[-2] + seq[-1]
+  def self.fib_iterative(n)
+    raise ArgumentError, "The number must be a positive integer" if n < 0
+    return [] if n == 0
+    return [0] if n == 1
+    return [0,1] if n == 2 
 
-  return seq
-end
-
-def self.fib_iterative(n)
-return ary[0...num] if num <= 2
-
-  ary = [0, 1] #index #-3 cz start in 0
-  (3..num).each { |i| ary << (ary[i - 2] + ary[i - 3])}
-  ary
-end
+    seq = [0, 1] 
+    (3..n).each { |i| seq << seq[-2] + seq[-1] }
+    seq
+  end
 
 #7------------------------------
-
-def self.merge_sort(array)
-  return array if array.length == 1
-  middle = array.length / 2
-  merge mergesort(array[0...middle]), mergesort(array[middle..-1])
-end
-
-def merge(left, right)
-  result = []
-  until left.length == 0 || right.length == 0 do
-    result << (left.first <= right.first ? left.shift : right.shift)
+  def self.merge_sort(array)
+    return array if array.length == 1
+    middle = array.length / 2
+    left = merge_sort (array[0...middle])
+    right = merge_sort (array[middle..-1])
+    merge(left, right)
   end
-  result + left + right
-end
 
+  def merge(left, right)
+    result = []
+    until left.length == 0 || right.length == 0 do
+        result << (left.first <= right.first ? left.shift : right.shift)
+    end
+    result + left + right
+  end
 #8------------------------------
-def self.binary_search(array, n)
+  def self.binary_search(array, n)
     min = 0
-  max = (array.length) - 1
-  loop_count = 0
+    max = (array.length) - 1
+    loop_count = 0
 
-  while min <= max
-    middle = (min + max) / 2
-    loop_count+=1
+    while min <= max
+      middle = (min + max) / 2
+      loop_count+=1
         
-    if array[middle] == n
-      print "#{loop_count} steps"
-      return middle
-    elsif array[middle] > n
-      max = middle - 1
-    elsif array[middle] < n
-      min = middle + 1
+      if array[middle] == n
+        print "#{loop_count} steps"
+        return middle
+      elsif array[middle] > n
+        max = middle - 1
+      elsif array[middle] < n
+        min = middle + 1
+      end 
     end 
-  end 
     print "#{n} not found in this array"
     return nil
-end 
+  end 
 
+  def self.binary_search_words(array, string)
+    min = 0
+    max = (array.length) - 1
+    loop_count = 0
 
+    while min <= max
+      middle = (min + max) / 2
+      loop_count+=1
+        
+      if array[middle] == string
+        print "#{loop_count} steps"
+        return middle
+      elsif array[middle] > string
+        max = middle - 1
+      elsif array[middle] < string
+        min = middle + 1
+      end 
+    end 
+    print "#{string} not found in this array"
+    return nil
+  end 
 
+end
+_____________________
+   
 a = Solution.new
 
   # 1
@@ -169,3 +191,14 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
   # 8
   Solution.binary_search([1,2,3,5], 5)
+  
+  #test 
+  require "open-uri"
+  require 'json'
+  file = open("https://gist.githubusercontent.com/jesseditson/1e6b2b524814320515ccfe7e2f856eda/raw/17d61fa1e80e14b13c4525b09f84148772586b59/words.json").read
+
+  data = JSON.parse(file)
+  test_dict = data["words"]
+  test_word = test_dict.sample
+
+  Solution.binary_search_words(test_dict, test_word)
